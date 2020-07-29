4d5a19eba7b9d2e094069697bae751112c108a33
Sravan <sravanjsc@gmail.com>
2020-07-29 13:30:52 +0530

    Added functions to reset the user password. Everything working fine.

--- ffgt/__init__.py
+++ ffgt/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '77bdb130b68e6a73c78b16cf493caa28'
@@ -11,5 +13,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'sravanjspro@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Heis101%Right'
+mail = Mail(app)
 
 from ffgt import routes
